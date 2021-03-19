89ce5e79a122d3ede4ffc5dcfdb6c3cb21e3aed1
Anil Kumar <anilbms75@gmail.com>
2021-03-19 23:53:22 +0100

    fix for 2.3

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,9 @@ console.log("Hey There! Welcome to My Portfolio Site");
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
-} else {
-  x.className = 'navtoggle';
-}
+  if(x.className == 'navtoggle') { 
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
