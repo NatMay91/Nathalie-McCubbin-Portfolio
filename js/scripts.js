96dd15a23182928ee7858a86912df5f0715d30e7
NatMay91 <79648413+NatMay91@users.noreply.github.com>
2021-03-19 20:28:10 +0100

    test.style.css , test.index.html and tezt.about.html added

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hey There! Welcome to My Portfolio Site");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
